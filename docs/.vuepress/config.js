e64da44e0458b68a00679d22bcdb639f55f17626
yellowsea <472534667@qq.com>
2020-03-25 22:57:47 +0800

    nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,19 @@
 module.exports = {
   title: 'Hello VuePress',
-  description: 'Just playing around'
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' }
+    ],
+    sidebar: [
+      {
+        title: 'Group 1', // 必要的
+        path: '/foo/', // 可选的, 应该是一个绝对路径
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1, // 可选的, 默认值是 1
+        children: ['/']
+      }
+    ]
+  }
 }
