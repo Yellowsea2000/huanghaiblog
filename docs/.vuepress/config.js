e3885077cc547e459425054935cb067e806c8e88
yellowsea2000 <472534667@qq.com>
2020-03-28 00:50:30 +0800

    sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,31 @@
 module.exports = {
   title: 'Yellowsea blog',
   base: '/huanghaiblog/',
+  extraWatchFiles: ['**/*.md', '**/*.vue', '**/*.js', '**/*.html'],
   themeConfig: {
     nav: [
-      { text: '前端', link: '/font' },
-      { text: 'webpack', link: '/webpack' },
+      { text: '前端', link: '/front/' },
       { text: 'git', link: '/git' }
     ],
-    sidebar: {
-      '/font': ['html.html']
-    }
+    sidebar: [
+      {
+        title: 'html',
+        path: '/front/',
+        collapsable: false,
+        sidebarDepth: 1
+      },
+      {
+        title: 'css',
+        path: '/front/css',
+        collapsable: false,
+        sidebarDepth: 1
+      },
+      {
+        title: 'js',
+        path: '/front/js',
+        collapsable: false,
+        sidebarDepth: 1
+      }
+    ]
   }
 }
